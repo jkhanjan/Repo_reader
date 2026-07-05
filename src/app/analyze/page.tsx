6243153aca2a16b185d2c88c4600f72f6bdd2d08
jkhanjan <jhakhanjan62@gmail.com>
2026-07-05 23:50:23 +0530

    extra route removed

--- src/app/analyze/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function AnalyzePage() {
-  return (
-    <div className="flex min-h-screen items-start justify-center px-4 pt-[5%]">
-      {/* <RepoInput /> */}
-    </div>
-  );
-}
